e74f54149bd18931b72c0762b54b8457e622bcaf
ANDREW <andrewklishch@gmail.com>
2022-05-30 20:44:33 +0300

    first commit

---
+++ CSS9/shared.js
@@ -0,0 +1,7 @@
+// alert ('This works');
+var backdrop = document.querySelector('.backdrop');
+var selectPlanButton = document.querySelectorAll('.plan button');
+//console.dir(button);
+// console.dir(backdrop);
+
+backdrop.style.display = 'block';
