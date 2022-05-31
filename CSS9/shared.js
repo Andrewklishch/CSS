dd250cad4571d9d78f4003a0ebaf7c43563dc70b
ANDREW <andrewklishch@gmail.com>
2022-05-31 11:39:43 +0300

    commit4

--- CSS9/shared.js
+++ CSS9/shared.js
@@ -1,7 +1,15 @@
 // alert ('This works');
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
 var selectPlanButton = document.querySelectorAll('.plan button');
+
 //console.dir(button);
 // console.dir(backdrop);
 
-backdrop.style.display = 'block';
+//backdrop.style.display = 'block';
+for (var i = 0; i < selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
