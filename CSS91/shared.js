b451b3f1e6705bec8fbe09ecf8f33eaa29b94c62
ANDREW <andrewklishch@gmail.com>
2022-05-31 12:53:25 +0300

    commit5

--- CSS91/shared.js
+++ CSS91/shared.js
@@ -1,11 +1,38 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
+
         backdrop.style.display = 'block';
+        modal.style.display = 'block';
     });
-}   
+}
+
+
+//backdrop.addEventListener('click', function() {
+backdrop.style.display = 'none';
+modal.style.display = 'none';
+//});
+
+//modalNoButton.addEventListener('click', function() {
+backdrop.style.display = 'none';
+modal.style.display = 'none';
+//});
+
+//function closeModal() {
+backdrop.style.display = 'none';
+modal.style.display = 'none';
+//}
+
+backdrop.addEventListener('click', closeModal);
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
